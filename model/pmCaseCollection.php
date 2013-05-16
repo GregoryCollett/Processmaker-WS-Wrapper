8f8aedf66494812fceb16bb3098406125d8e36b6
Greg Davies <greg@Greg-TestPC.(none)>
2013-05-16 17:38:03 +0100

    Works in progress.

---
+++ model/pmCaseCollection.php
@@ -0,0 +1,13 @@
+<?php
+
+class pmCaseCollection {
+	$cases = array();
+	function populate($caseArray) {
+		print_r(array_keys($caseArray));
+		foreach($caseArray as $caseRow) {
+		}
+		//$this->cases
+	}
+}
+
+?>
