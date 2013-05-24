2969a981d70cc3e5c424c80784511d56854cf8c4
Greg <gregmichaeldavies@gmail.com>
2013-05-24 11:53:52 +0100

    Included getVariables functionality.

--- model/pmCase.php
+++ model/pmCase.php
@@ -1,16 +1,23 @@
 <?php
 
+require_once('variableStruct.php');
+
 class pmCase {
 	public $guid, $name, $status, $delIndex;
-	public $task, $docs, $variables;
+	public $task, $docs, $variables, $info;
 	function fetchTaskCase(pmConnect $connection) {
 		$this->task = $connection->get_task_case($this->guid);
 	}
 	function fetchDocList(pmConnect $connection) {
 		$this->docs = $connection->output_doc_list($this->guid);
 	}
-	function getVariables(pmConnect $connection) {
-		$this->variables = $connection->get_variables($this->guid);
+	function fetchVariables(pmConnect $connection, $variables = null) {
+		$vars = array();
+		foreach($variables as $variable) {$vars[] = new variableStruct($variable);}
+		$this->variables = $connection->get_variables($this->guid, $vars);
+	}
+	function fetchCaseInfo(pmConnect $connection) {
+		$this->info = $connection->get_case_info($this->guid, 0);
 	}
 
 }
---
+++ model/variableStruct.php
@@ -0,0 +1,8 @@
+<?php
+
+class variableStruct {
+	public $name;
+	function __construct($name) {$this->name = $name;}
+}
+
+?>
