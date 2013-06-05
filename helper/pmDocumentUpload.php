bf414afb021eaa4a0e62106b021e28024112ebec
GregoryCollett <gregory@gointeractive.co.uk>
2013-06-05 17:04:17 +0100

    document uploads

--- helper/pmDocumentUpload.php
+++ helper/pmDocumentUpload.php
@@ -1,68 +1,88 @@
 <?php
 
 class pmDocumentUpload {
-	
-	// Keep this stuff private man...
-	private $process, $case, $task, $user, $doc, $response;
 
-	/**
-	* Constructor
-	*/
-	public function pmDocumentUpload() {
+    // Keep this stuff private man...
+    private $process, $caseID, $file, $response;
 
-	}
+    /**
+     * Constructor
+     */
+    public function __construct($caseID = null, $file = null) {
+        if ($caseID) {
+            $this->setCaseID($caseID);
+        }
 
-	// Generate a random document id (preferably a guid based on the doc title)
-	private function generateDocId() {
-	
-	}
+        if ($file) {
+            $this->setFile($file);
+        }
+    }
 
-	// Set the document.. (Expects an obj)
-	public function setDoc($document) {
-		$this->doc = $document
-	}
+    // Set the document.. (Expects an obj)
+    public function setFile($document) {
+        $this->file = $document;
+        return $this;
+    }
 
-	/**
-	* Upload that doc!
-	*/
-	public function upload($document) {
-		$this->request("ATTACHED");
-	}
+    public function setCaseID($caseID) {
+        $this->caseID = $caseID;
+        return $this;
+    }
 
-	/**
-	* Curl Requests...
-	*/
-	public function request($doc_type = null, $ssl = false) {
-		if ( !$doc_type ) {
-			$doc_type = "ATTACHED";
-		}
-		$c_init = curl_init();
-		$params = array (
-		    'ATTACH_FILE'  => '@' . $file_path,
-		    'APPLICATION'  => $this->case->id,
-		    'INDEX'        => 1,
-		    'USR_UID'      => $this->user->id,
-		    'DOC_UID'      => '547CEBEE61202A',
-		    'APP_DOC_TYPE' => $doc_type,
-		    'TITLE'        => $this->doc->title,
-		    'COMMENT'      => (isset($this->doc->comment)) ? $this->doc->comment : "";
-		 );
-		 ob_flush();
-		 $c_init = curl_init();
-		 curl_setopt($c_init, CURLOPT_URL, 'http://192.168.1.100/sysworkflow/en/green/services/upload');
-		 // curl_setopt($c_init, CURLOPT_VERBOSE, 1);  //Uncomment to debug
-		 curl_setopt($c_init, CURLOPT_RETURNTRANSFER, 1);
-		 curl_setopt($c_init, CURLOPT_POST, 1);
-		 curl_setopt($c_init, CURLOPT_POSTFIELDS, $params);
+    public function getCaseID() {
+        return $this->caseID;
+    }
 
-		 if ($ssl) {
-		 	curl_setopt ($c_init, CURLOPT_SSL_VERIFYHOST, 1); //Uncomment for SSL
-		 	curl_setopt ($c_init, CURLOPT_SSL_VERIFYPEER, 1); //Uncomment for SSL
-		 }
-		 $response = curl_exec($c_init);
-		 curl_close($c_init);
-		 print "<HTML><BODY><PRE>$response</PRE></BODY></HTML>";
-	}
+    /**
+     * Upload that doc!
+     */
+    public function upload() {
+        $this->request();
+    }
 
+    /**
+     * Curl Requests...
+     */
+    public function request($doc_type = null, $ssl = false) {
+        if (!$doc_type) {
+            $doc_type = "ATTACHED";
+        }
+        $params = array(
+            'ATTACH_FILE' => '@/home/four4long/test.txt',
+            'APPLICATION' => $this->caseID,
+            'INDEX' => 1,
+            'USR_UID' => '00000000000000000000000000000001',
+            'DOC_UID' => '-1',
+            'APP_DOC_TYPE' => $doc_type,
+            'TITLE' => 'Test Doc',
+            'COMMENT' => (isset($this->doc->comment)) ? $this->doc->comment : ""
+        );
+        ob_flush();
+        $c_init = curl_init();
+        curl_setopt($c_init, CURLOPT_URL, 'http://151.236.221.19/sysworkflow/en/uxmodern/services/upload');
+        // curl_setopt($c_init, CURLOPT_VERBOSE, 1);  //Uncomment to debug
+        curl_setopt($c_init, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c_init, CURLOPT_POST, 1);
+        curl_setopt($c_init, CURLOPT_POSTFIELDS, $params);
+
+        if ($ssl) {
+            curl_setopt($c_init, CURLOPT_SSL_VERIFYHOST, 1); //Uncomment for SSL
+            curl_setopt($c_init, CURLOPT_SSL_VERIFYPEER, 1); //Uncomment for SSL
+        }
+        $response = curl_exec($c_init);
+        curl_close($c_init);
+        
+        $this->setResponse($response);
+        return $this->response;
+    }
+    
+    private function setResponse($response) {
+        $this->response = $response;
+    }
+    
+    public function getResponse() {
+        return $this->response;
+    }
 }
+
 ?>
