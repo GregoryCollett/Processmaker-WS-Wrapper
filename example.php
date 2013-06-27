784cef400e46f67487834717d1c3aee5e169ec47
Greg <gregmichaeldavies@gmail.com>
2013-06-27 16:38:22 +0100

    Example includes trigger execution

--- example.php
+++ example.php
@@ -10,13 +10,17 @@ $case = $collection->getIterator()->current();
 $case->fetchTaskCase($pm);
 echo "Done. ".curDate()."\nDoc list... ";
 $case->fetchDocList($pm);
+echo "Done. ".curDate()."\nExecuting Trigger... ";
+$case->executeTrigger($pm, "259309785519f84f5627af5016556096");
 echo "Done. ".curDate()."\nVariables... ";
 $case->fetchVariables($pm, array(
 	//"EMail",
 	//"Gender",
 
-	"form[MainContactDetails][1][EMail]",
-	"form[Additional_Contact][1][Gender]",
+	"MCDSerialized",
+	"MCDSerialized2",
+	//"form[MainContactDetails][1][EMail]",
+	//"form[Additional_Contact][1][Gender]",
 ));
 echo "Done. ".curDate()."\nCase Info... ";
 $case->fetchCaseInfo($pm);
